17987d08b28fbe86dfcfff7008bd86daf608c1da
web4abdessattar <abdessattar.bahri@ensi-uma.tn>
2016-07-31 18:51:24 +0200

    stoura

--- htdocs/controllers/kernel.php
+++ htdocs/controllers/kernel.php
@@ -3,16 +3,17 @@
 //Load the libraries
 include_once('./libraries/php/Request.class.php');
 include_once('./configurations/database.php');
-include_once('./libraries/php/dao/ProjectsDAO.php');
+//include_once('./libraries/php/dao/ProjectsDAO.php');
 
 $ctl = Request::getController();
+
 
 if(empty($ctl)){
   $ctl = 'home';
 }
 if(!file_exists('controllers/'. $ctl .'.php')){
   $ctl = 'home';
-}else {
-  include('controllers/'. $ctl .'.php');
 }
+  include('controllers/'. $ctl .'.php');
+
  ?>
---
+++ htdocs/controllers/sign_in.php
@@ -0,0 +1,6 @@
+<?php
+
+  include_once("./views/home/sign_in.php");
+  //include once is better in performance because (file exists in the cache) but a problem if we change the file and the cache havn t change
+  //include == require
+ ?>
